f305f8d942a6d1d1c02a4ca316e462ca75e815fb
Walden-Shen <41880356@qq.com>
2017-04-22 21:08:11 +0800

    the first nine lectures

--- myExercise.py
+++ myExercise.py
@@ -84,3 +84,68 @@ def count_partitions(n, m):
         return 0
     else:
         return count_partitions(n - m, m) + count_partitions(n, m - 1)
+
+"""
+5.GCD and function descriptor
+"""
+def gcd(m, n):
+    if m % n == 0:
+        return n
+    elif m < n:
+        return gcd(n, m)
+    else:
+        return gcd(m - n, n)
+
+def trace(f):
+    def traced(m, n):
+        print('Called on ', m, 'and', n)
+        return f(m, n)
+    return traced
+"""
+6.silly handout concerning recursion
+"""
+def expt(base, power):
+    """
+    >>> expt(2, 3)
+    8
+    >>> expt(2, -1)
+    0.5
+    """
+    if power == 1:
+        return base
+    elif power == 0:
+        return 1
+    elif power > 1:
+        return expt(base, power - 1) * base
+    else:
+        return expt(base, power + 1) / base
+
+def count_stair_ways(n):
+    if n >= 1:
+        return count_stair_ways(n - 1) + count_stair_ways(n - 2) 
+    elif n == 0:
+        return 1
+    else:
+        return 0
+
+def pascal(row, column):
+    if column == 0:
+        return 1
+    elif column == row:
+        return 1
+    else:
+        return pascal(row - 1, column - 1) + pascal(row - 1, column)
+
+def has_sum(sum, n1, n2):
+    """
+    >>> has_sum(1, 3, 5)
+    False
+    >>> has_sum(5, 3, 5)
+    True
+    """
+    if sum < 0:
+        return False
+    elif sum % n1 == 0 or sum % n2 == 0:
+        return True
+    else:
+        return has_sum(sum - n1, n1, n2) or has_sum(sum - n2, n1, n2)
