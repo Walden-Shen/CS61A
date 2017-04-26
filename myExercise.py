110bd8baf643440e1ef0e6e49ed6369087555817
Walden-Shen <41880356@qq.com>
2017-04-26 12:14:00 +0800

    finish discussion 4 : tree and sequence

--- myExercise.py
+++ myExercise.py
@@ -230,18 +230,82 @@ def square_tree(tree):
 #return the height of a Tree
 def height(tree):
     if is_leaf(tree):
-        return
+        return 1
     else:
-        return max([branch + 1 for branch in branches(tree)])
+        return max([height(branch) + 1 for branch in branches(tree)])
 
 #return the number of nodes
 def tree_size(tree):
     if is_leaf(tree):
         return 1
     else:
-        return 1 + tree_size(branches(tree))
+        return 1 + tree_size([branch] for branch in branches(tree))
 
+#return the max number in the Tree
+def tree_max(tree):
+    if is_leaf(tree):
+        return root(tree)
+    else:
+        return max([root(tree)] + [tree_max(branch) for branch in
+            branches(tree)])
 
+#expression Tree
+from operator import add, mul
+def eval_tree(tree):
+    """Evaluates an expression tree with functions as root
+    >>> eval_tree(tree(1))
+    1
+    >>> expr = tree(mul, [tree(2), tree(3)])
+    >>> eval_tree(expr)
+    6
+    >>> eval_tree(tree(add, [expr, tree(4)]))
+    10
+    """
+    if not is_leaf(tree):
+        return root(tree)(eval_tree(tree[1]), eval_tree(tree[2]))
+    else:
+        return root(tree)
+
+#hailstone tree
+def hailstone_tree(n, height):
+    """Generates a tree of hailstone numbers that will reach N
+    , with height H.
+    >>> hailstone_tree(1, 0)
+    [1]
+    >>> hailstone_tree(1, 4)
+    [1, [2, [4, [8, [16]]]]]
+    >>> hailstone_tree(8, 3)
+    [8, [16, [32, [64]], [5, [10]]]]
+    """
+    if height == 0:
+        return [n]
+    elif (float)((n - 1) / 3) == (int)((n - 1) // 3) and (n - 1) // 3 % 2 == 1 and (n - 1) // 3 != 1:
+        return [n] + [hailstone_tree(2 * n, height - 1)] + [hailstone_tree((n -
+            1) // 3, height - 1)]
+    else:
+        return [n] + [hailstone_tree(2 * n, height - 1)]
+#find path in a tree assuming all the value in the tree is unique
+def find_path(tree, x):
+    """ Return a path in a tree to a leaf with value X,
+    None if such a leaf is not present.
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])]), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 6)
+    [2, 7, 6]
+    >>> find_path(t, 10)
+    """
+    global temp
+    if root(tree) == x:
+        temp = []
+        return True
+    elif is_leaf(tree):
+        return False
+    else:
+        for branch in branches(tree):
+            if find_path(branch, x):
+                temp = [root(branch)] + temp
+                return [root(tree)] + temp
 """
 8. Function vary in different environment
 """
