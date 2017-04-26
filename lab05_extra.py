180f8e67f6846704671207731c54ee313454d4b7
Walden-Shen <41880356@qq.com>
2017-04-26 14:39:46 +0800

    dull && simple lab...

---
+++ lab05_extra.py
@@ -0,0 +1,63 @@
+from lab05 import *
+################
+# Dictionaries #
+################
+
+# Q8
+def build_successors_table(tokens):
+    """Return a dictionary: keys are words; values are lists of
+    successors.
+
+    >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
+    >>> table = build_successors_table(text)
+    >>> sorted(table)
+    [',', '.', 'We', 'and', 'bad', 'came', 'catch', 'eat', 'guys', 'investigate', 'pie', 'to']
+    >>> table['to']
+    ['investigate', 'eat']
+    >>> table['pie']
+    ['.']
+    >>> table['.']
+    ['We']
+    """
+    table = {}
+    prev = '.'
+    for word in tokens:
+        if prev not in table:
+            "*** YOUR CODE HERE ***"
+            table[prev] = []
+        "*** YOUR CODE HERE ***"
+        table[prev].append(word)
+        prev = word
+    return table
+
+# Q9
+def construct_sent(word, table):
+    """Prints a random sentence starting with word, sampling from
+    table.
+    """
+    import random
+    result = ' '
+    while word not in ['.', '!', '?']:
+        "*** YOUR CODE HERE ***"
+        result += word + ' '
+        word = table[word][sample(table[word], 1)]
+    return result + word
+
+# Warning: do NOT try to print the return result of this function
+def shakespeare_tokens(path='shakespeare.txt', url='http://goo.gl/SztLfX'):
+    """Return the words of Shakespeare's plays as a list."""
+    import os
+    from urllib.request import urlopen
+    if os.path.exists(path):
+        return open('shakespeare.txt', encoding='ascii').read().split()
+    else:
+        shakespeare = urlopen(url)
+        return shakespeare.read().decode(encoding='ascii').split()
+
+# Uncomment the following two lines
+# tokens = shakespeare_tokens()
+# table = build_successors_table(tokens)
+
+def random_sent():
+    import random
+    return construct_sent(random.choice(table['.']), table)
