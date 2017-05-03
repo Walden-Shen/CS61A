8cb8d3b90d69f07b1823cf7afe7b99656672d775
Walden-Shen <41880356@qq.com>
2017-05-03 19:01:49 +0800

    the last question of this lab is a mess. Both i am unable to figure out its meaning and the standard answer is error

--- lab09/lab09-extra.scm
+++ lab09/lab09-extra.scm
@@ -1,24 +1,39 @@
 (define (composed f g)
-  'YOUR-CODE-HERE
+ (lambda (x) (f (g x)))
 )
 
 (define (max a b) (if (> a b) a b))
 (define (min a b) (if (> a b) b a))
 (define (gcd a b)
   'YOUR-CODE-HERE
+  (cond ((or (= a 0) (= b 0)) (max a b))
+   		((= (modulo (max a b) (min a b)) 0) (min a b))
+   		(else (gcd(min a b) (modulo (max a b) (min a b)))))
 )
 
 (define (filter f lst)
   'YOUR-CODE-HERE
+  (cond ((null? lst) nil)
+   		((f (car lst)) (cons (car lst) (filter f (cdr lst))))
+   		(else (filter f (cdr lst))))
 )
 
 (define (all-satisfies lst pred)
   'YOUR-CODE-HERE
+  (cond ((null? lst) #t)
+   		((pred (car lst)) (or (all-satisfies (cdr lst) pred) #t))
+		(else #f))
+  'standard version (= (length (filter pred lst) (length lst)))
 )
 
 (define (accumulate combiner start n term)
   (if (= n 0)
       start
       'YOUR-CODE-HERE
+	  (combiner (accumulate combiner
+				 			start
+							(- n 1)
+							term)
+	   			(term n))
       ))
 
