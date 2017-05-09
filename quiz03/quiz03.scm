aa7bb42280f8d2c9ab763c10e6a9fde70c4562de
Walden-Shen <41880356@qq.com>
2017-05-09 14:03:45 +0800

    finished

--- quiz03/quiz03.scm
+++ quiz03/quiz03.scm
@@ -3,24 +3,36 @@
 
 (define (map f s)
   ; List the result of applying f to each element in s.
-  (if (null? s) s
-    (cons (f (car s)) (map f (cdr s)))))
+  (cond ((null? s) s)
+   		((number? s) (f s))
+    	(else (cons (f (car s)) (map f (cdr s))))))
 
 (define (filter f s)
   ; List the elements of s for which f returns a true value.
-  (if (null? s) s
-    (let ((rest (filter f (cdr s))))
-      (if (f (car s)) (cons (car s) rest) rest))))
+  (cond ((null? s) s)
+   		((boolean? s) (f s))
+		(else (let ((rest (filter f (cdr s))))
+      		(if (f (car s)) (cons (car s) rest) rest)))))
 
 (define (no-repeats s)
   ; YOUR-CODE-HERE
   (define (equal x) (= (car s) x))
-  (define (wrong x) (if (equal? #t x) #t #f))
   (cond ((equal? s nil) nil)
    		((equal? (filter wrong (map equal (cdr s))) nil) (cons (car s) (no-repeats (cdr s))))
 		(else (no-repeats (cdr s))))
   )
+(define (wrong x) (if (equal? #t x) #t #f))
+
+(define (dot x) (cond 	((number? x) #f)
+				 		((number? (cdr x)) #t)
+						((pair? (cdr x)) (dot (cdr x)))
+						(else #f)))
+
+(define (len x) (cond ((boolean? x) 1)
+				 		((null? x) 0)
+				 		(else (+ 1 (len (cdr x))))))
 
 (define (how-many-dots s)
   ; YOUR-CODE-HERE
-  'replace-this)
+  (len (filter wrong (map dot s)))
+)
