9770c562a414996ab3438fab8e7fd49ca34347db
Walden-Shen <41880356@qq.com>
2017-05-09 16:17:52 +0800

    finished

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,17 +1,29 @@
 
 (define (deep-map fn s)
   ; YOUR-CODE-HERE
-  nil
+  (cond ((null? s) nil)
+   		((list? (car s)) (cons (deep-map fn (car s)) (deep-map fn (cdr s))))
+  		(else (cons (fn (car s)) (deep-map fn (cdr s)))))
 )
 
 (define (substitute s old new)
   ; YOUR-CODE-HERE
-  nil
+  (define (replace x) (if (eq? old x) new x))
+  (deep-map replace s)
 )
 
+(define (deep-map-two fn s olds news)
+  ; YOUR-CODE-HERE
+  (cond ((null? s) nil)
+   		((list? (car s)) (cons (deep-map-two fn (car s) olds news) (deep-map-two fn (cdr s) olds news)))
+  		(else (cons (fn (car s) olds news) (deep-map-two fn (cdr s) olds news))))
+)
 (define (sub-all s olds news)
   ; YOUR-CODE-HERE
-  nil
+  (define (replace x olds news) (cond ((null? olds) x)
+					   				((eq? x (car olds)) (car news))
+									(else (replace x (cdr olds) (cdr news)))))
+  (deep-map-two replace s olds news)
 )
 
 
