79c2ea2dce117ad3e8ce57a200a0e78c2a85b622
Walden-Shen <41880356@qq.com>
2017-05-12 19:00:36 +0800

    finished

--- lab13/lab13.sql
+++ lab13/lab13.sql
@@ -11,14 +11,14 @@ DROP TABLE IF EXISTS likeliest_child;
 -- A table containing words that are masculine but end in "a"
 CREATE TABLE masculine_a as
     -- REPLACE THIS LINE
-    select 'YOUR CODE HERE';
+    select word from deps where substr(word, -1) = 'a' and dependent_word = 'el';
 
 -- A table containing each word, category, and count of that group
 CREATE TABLE word_cat_count AS
     -- REPLACE THIS LINE
-    select 'YOUR CODE HERE';
+    select word, category, count(*) as count from deps group by category, word;
 
 -- A table containing all info of words that can be both nouns and verbs
 CREATE TABLE noun_verbs AS
     -- REPLACE THIS LINE
-    select 'YOUR CODE HERE';
+    select a.word, a.category, a.count, b.word, b.category, b.count from word_cat_count as a, word_cat_count as b where a.word = b.word and a.category = 'NC' and b.category like 'V__';
